f2651af424cd796194b8c29ae682aad3aae60edb
Atsuya Nagahara <atsuya@debian>
2017-04-05 10:54:29 +0900

    Add a full_title helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+  #ページごとの完全なタイトルを返します。    #コメント
+  def full_title(page_title = '')            #メソッドの定義と引数
+    base_title = "TokiNeji.Site"             #ベースタイトル
+    if page_title.empty?                     #タイトルは空か？
+      base_title                             #ベースタイトル
+    else                                     #ありなら
+      page_title + " | " + base_title        #ページタイトルとベースタイトル
+    end
+  end
 end
